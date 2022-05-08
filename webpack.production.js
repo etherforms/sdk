98a11a72f67408bad3cd9111f9b0cae58ebd6e06
--- <4408878+stoll@users.noreply.github.com>
2022-05-09 00:16:41 +0200

    Add SCSS to production builds

--- webpack.production.js
+++ webpack.production.js
@@ -5,10 +5,32 @@ const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = merge(common, {
     mode: "production",
     plugins: [new CompressionPlugin()],
-    entry: "./src/index.ts",
+    entry: [
+        __dirname + "/src/index.ts",
+        __dirname + "/src/scss/etherforms.scss"
+    ],
     resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".scss"],
         modules: ["src", "node_modules"] // Assuming that your files are inside the src dir
     },
-    devtool: "source-map"
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [],
+            }, {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: { outputPath: "/", name: "[name].min.css" }
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
 });
